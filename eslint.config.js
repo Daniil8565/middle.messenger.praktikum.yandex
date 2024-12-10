22695412e4ff8754c6b8513004f803d2f8760dd3
Daniil Pereskokov <129772326+Daniil8565@users.noreply.github.com>
2024-12-10 12:17:26 +0300

    6

---
+++ eslint.config.js
@@ -0,0 +1,15 @@
+import parsel from "@typescript-eslint/parser";
+import eslintPlugin from "@typescript-eslint/eslint-plugin";
+
+export default [
+  {
+    files: ["**/*.ts"],
+    ignores: ["/node_modules", "/github", "/dist"],
+    languageOptions: {
+      parser: parsel,
+    },
+    plugins: {
+      "@typescript-eslint": eslintPlugin,
+    },
+  },
+];
