4ebcebfa75311213f12570dcbd723fc37c413df5
Daniil Pereskokov <129772326+Daniil8565@users.noreply.github.com>
2024-12-10 12:09:37 +0300

    3

--- mochaSetup.js
+++ mochaSetup.js
@@ -13,7 +13,3 @@ const jsdom = new JSDOM(
 
 global.window = jsdom.window;
 global.document = jsdom.window.document;
-// global.Node = jsdom.window.Node;
-// global.history = jsdom.window.history;
-// global.XMLHttpRequest = jsdom.window.XMLHttpRequest;
-// global.MouseEvent = jsdom.window.MouseEvent;
